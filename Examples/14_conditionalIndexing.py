72bf79d0fd95c248c7b4288c0706040dcd7edf15
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 11:25:51 +0600

    Create 14_conditionalIndexing.py

---
+++ Examples/14_conditionalIndexing.py
@@ -0,0 +1,10 @@
+# Example 14: NumPy - Conditional Indexing
+import numpy as np
+
+# Creating an array
+array = np.array([1, 2, 3, 4, 5, 6])
+
+# Setting elements that satisfy a condition
+array[array % 2 == 0] = -1  # Set even numbers to -1
+
+print('Array after Conditional Indexing:', array)
