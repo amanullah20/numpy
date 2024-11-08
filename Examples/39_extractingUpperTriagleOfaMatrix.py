e88a453be78ef92970592129b996948d4daa4ee3
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 13:50:17 +0600

    Create 39_extractingUpperTriagleOfaMatrix.py

---
+++ Examples/39_extractingUpperTriagleOfaMatrix.py
@@ -0,0 +1,10 @@
+# Example 39: NumPy - Extracting Upper Triangle of a Matrix
+import numpy as np
+
+# Creating a square matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Extracting the upper triangle (above the main diagonal)
+upper_triangle = np.triu(matrix)
+
+print('Upper Triangle of the Matrix:\n', upper_triangle)
