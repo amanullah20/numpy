d44e91d01df849d9184512c70fc9aacee0a14073
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 14:12:48 +0600

    Create 48_using_npChoose_for_multiple_conditions.py

---
+++ Examples/48_using_npChoose_for_multiple_conditions.py
@@ -0,0 +1,11 @@
+ # Example 48: NumPy - Using np.choose for Multiple Conditions
+import numpy as np
+
+# Creating arrays for conditions
+array = np.array([0, 1, 2, 3])
+choices = [array*2, array+10, array**2]
+
+# Using np.choose to apply different conditions
+result = np.choose(array, choices)
+
+print('Result using np.choose:', result)
