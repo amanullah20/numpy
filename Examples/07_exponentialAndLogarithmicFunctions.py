eaf8a410f0ad45cc81b51297fc8e625e0a523a63
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 11:17:12 +0600

    Create 07_exponentialAndLogarithmicFunctions.py

---
+++ Examples/07_exponentialAndLogarithmicFunctions.py
@@ -0,0 +1,12 @@
+# Example 07: NumPy - Exponential and Logarithmic Functions
+import numpy as np
+
+# Creating an array
+array = np.array([1, 2, 3])
+
+# Exponential and logarithmic functions
+exp_array = np.exp(array)
+log_array = np.log(array)
+
+print('Exponential of Array:', exp_array)
+print('Logarithm of Array:', log_array)
