e7a1bcdeeb4a2914cb1ebba6108dd6bce9953964
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 12:22:54 +0600

    Create 28_concatenationOfArrays.py

---
+++ Examples/28_concatenationOfArrays.py
@@ -0,0 +1,11 @@
+# Example 28: NumPy - Concatenation of Arrays
+import numpy as np
+
+# Creating arrays
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+# Concatenating arrays
+concatenated_array = np.concatenate((array1, array2))
+
+print('Concatenated Array:', concatenated_array)
