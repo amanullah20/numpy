e7c1e0aba52a2e3d91b30780855d8485c361d85a
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 12:21:29 +0600

    Create 27_uniqueElementsAndCounting.py

---
+++ Examples/27_uniqueElementsAndCounting.py
@@ -0,0 +1,11 @@
+# Example 27: NumPy - Unique Elements and Counting
+import numpy as np
+
+# Creating an array with duplicate values
+array = np.array([1, 2, 2, 3, 4, 4, 4, 5])
+
+# Finding unique elements and their counts
+unique_elements, counts = np.unique(array, return_counts=True)
+
+print('Unique Elements:', unique_elements)
+print('Counts of Unique Elements:', counts)
