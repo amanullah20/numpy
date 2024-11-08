270bd4efd311192cc78862fe405995b759f0c2dc
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 13:53:01 +0600

    Create 41_extractLowerTriangleOfMatrix.py

---
+++ Examples/41_extractLowerTriangleOfMatrix.py
@@ -0,0 +1,10 @@
+# Example 41: NumPy - Extract Lower Triangle of a Matrix
+import numpy as np
+
+# Creating a square matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Extracting the lower triangle (below the main diagonal)
+lower_triangle = np.tril(matrix)
+
+print('Lower Triangle of the Matrix:\n', lower_triangle)
