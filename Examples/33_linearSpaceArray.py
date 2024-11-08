31727f6ac5c8143b239e4f5141de0c8d38b2fd14
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 12:51:53 +0600

    Create 33_linearSpaceArray.py

---
+++ Examples/33_linearSpaceArray.py
@@ -0,0 +1,7 @@
+# Example 33: NumPy - Linear Space Array
+import numpy as np
+
+# Generating a linear space array with evenly spaced values
+linear_space = np.linspace(0, 10, 5)
+
+print('Linear Space Array:', linear_space)
