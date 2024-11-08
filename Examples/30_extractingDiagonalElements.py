109a8515256574d974e360fd30392436d4749a3d
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 12:47:21 +0600

    Create 30_extractingDiagonalElements.py

---
+++ Examples/30_extractingDiagonalElements.py
@@ -0,0 +1,10 @@
+# Example 30: NumPy - Extracting Diagonal Elements
+import numpy as np
+
+# Creating a square matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Extracting the main diagonal
+diagonal_elements = np.diag(matrix)
+
+print('Diagonal Elements:', diagonal_elements)
