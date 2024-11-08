a721b3bcd4beb5b85645dc88f2795a6802ce3fd3
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 11:18:16 +0600

    Create 08_fancyIndexing.py

---
+++ Examples/08_fancyIndexing.py
@@ -0,0 +1,10 @@
+# Example 08: NumPy - Fancy Indexing
+import numpy as np
+
+# Creating an array
+array = np.array([10, 20, 30, 40, 50])
+
+# Selecting specific indices
+selected_elements = array[[1, 3, 4]]
+
+print('Selected Elements:', selected_elements)
