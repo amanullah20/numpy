96379cb560cc15856bd44238ee41fed6ce9ebcb3
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 12:09:55 +0600

    Create 25_statisticalFunctions.py

---
+++ Examples/25_statisticalFunctions.py
@@ -0,0 +1,16 @@
+# Example 25: NumPy - Statistical Functions
+import numpy as np
+
+# Creating an array
+array = np.array([1, 2, 3, 4, 5])
+
+# Calculating statistical values
+mean = np.mean(array)
+median = np.median(array)
+std_dev = np.std(array)
+variance = np.var(array)
+
+print('Mean:', mean)
+print('Median:', median)
+print('Standard Deviation:', std_dev)
+print('Variance:', variance)
