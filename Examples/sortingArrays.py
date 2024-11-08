d7fd09e73c84f53a01d2fe40d804c5461bb56345
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 11:30:54 +0600

    Create sortingArrays.py

---
+++ Examples/sortingArrays.py
@@ -0,0 +1,11 @@
+# Example 19: NumPy - Sorting Arrays
+import numpy as np
+
+# Creating an array
+array = np.array([3, 1, 2, 5, 4])
+
+# Sorting the array
+sorted_array = np.sort(array)
+
+print('Original Array:', array)
+print('Sorted Array:', sorted_array)
