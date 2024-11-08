55d384cb2b89f72987d845689b876be0a1cff7f3
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 11:37:09 +0600

    Create 22_booleanIndexing.py

---
+++ Examples/22_booleanIndexing.py
@@ -0,0 +1,11 @@
+# Example 22: NumPy - Boolean Indexing
+import numpy as np
+
+# Creating an array
+array = np.array([1, 2, 3, 4, 5, 6])
+
+# Boolean indexing
+even_numbers = array[array % 2 == 0]
+
+print('Original Array:', array)
+print('Even Numbers:', even_numbers)
