143c46b3bbff8adccd76085d6e11c6b10d9daed7
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 12:59:34 +0600

    Create 35_meshgridCreation.py

---
+++ Examples/35_meshgridCreation.py
@@ -0,0 +1,12 @@
+# Example 35: NumPy - Meshgrid Creation
+import numpy as np
+
+# Creating 1D arrays
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+
+# Creating a meshgrid
+X, Y = np.meshgrid(x, y)
+
+print('Meshgrid X:\n', X)
+print('Meshgrid Y:\n', Y)
