46caf196e33635ba4689a30670b74fb6cb51ce42
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 11:15:08 +0600

    Create 06_powerAndSquareRoot.py

---
+++ Examples/06_powerAndSquareRoot.py
@@ -0,0 +1,12 @@
+# Example 06: NumPy - Power and Square Root
+import numpy as np
+
+# Creating an array
+array = np.array([1, 4, 9, 16])
+
+# Power and square root
+squared = np.power(array, 2)
+sqrt = np.sqrt(array)
+
+print('Squared Array:', squared)
+print('Square Root of Array:', sqrt)
