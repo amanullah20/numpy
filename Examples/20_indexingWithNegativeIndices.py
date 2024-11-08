5e7933d1c4f1b08642b87842d97c8221f10bf00a
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 11:32:39 +0600

    Create 20_indexingWithNegativeIndices.py

---
+++ Examples/20_indexingWithNegativeIndices.py
@@ -0,0 +1,12 @@
+# Example 20: NumPy - Indexing with Negative Indices
+import numpy as np
+
+# Creating an array
+array = np.array([10, 20, 30, 40, 50])
+
+# Accessing elements from the end using negative indices
+last_element = array[-1]
+second_last_element = array[-2]
+
+print('Last Element:', last_element)
+print('Second Last Element:', second_last_element)
