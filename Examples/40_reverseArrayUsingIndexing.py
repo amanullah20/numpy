f41fbf3c2a278d2c8734901e6f737d18dafda01d
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-11-08 13:51:25 +0600

    Create 40_reverseArrayUsingIndexing.py

---
+++ Examples/40_reverseArrayUsingIndexing.py
@@ -0,0 +1,10 @@
+# Example 40: NumPy - Reverse Array using Indexing
+import numpy as np
+
+# Creating an array
+array = np.array([1, 2, 3, 4, 5])
+
+# Reversing the array using slicing
+reversed_array = array[::-1]
+
+print('Reversed Array:', reversed_array)
